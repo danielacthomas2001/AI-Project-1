436ab162abc8c62ac0ab5d10ef82734d5dd640a2
danielacthomas2001 <111799245+danielacthomas2001@users.noreply.github.com>
2023-04-09 15:26:15 -0400

    Add files via upload
    Added a Streamlit app that analyzes sentiment of text using pre-trained models from the Transformers library.

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from transformers import pipeline
+
+
+#title
+st.title("Sentiment Text Analysis App")
+
+#subtitle
+
+st.markdown("## Sentiment Analyis App - Using 'streamlit'")
+st.markdown("")
+st.markdown("")
+st.write("")
+#brief description of app
+st.write("Input a text and choose a pre-trained model to obtain its corresponding sentiment analysis. ")
+
+default_text = "All those moments will be lost in time, like tears in rain."
+
+
+with st.form(key='my_form'):
+    text = st.text_input("Enter Your Text Here: ", value = default_text)
+    model_name = ...
+    submit_button = st.form_submit_button(label = 'Submit')
+
+def sentiment_analysis(text, model_name):
+
+    #Loads the selected model
+    setiment_model = pipeline('sentiment-analysis', model = model_name )
+    #performs the analysis
+    result = setiment_model(text)[0]
+    sentiment = result['label']
+    score = result['score']
+
+    return sentiment, score
+
+if submit_button:
+    if text:
+        sentiment, score = sentiment_analysis(text, model_name)
+        st.write('Sentiment: ' + sentiment)
+        st.write('Score: ' + score)
+        
+
+
