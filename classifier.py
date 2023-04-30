a5ee93408127b0b0621135790e3439ed8a32b37b
danielacthomas2001 <111799245+danielacthomas2001@users.noreply.github.com>
2023-04-30 15:36:16 -0400

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,10 +1,17 @@
+
+'''
+This code is for training and evaluating a DistilBERT-based model for multi-label classification of toxic comments. 
+The data is loaded, split into training and validation sets, tokenized, and converted into custom PyTorch datasets. 
+Then, the pre-trained model is fine-tuned using the Hugging Face Trainer class, and finally, the tokenizer and model are 
+saved and reloaded for evaluation.
+'''
 import numpy as np
 import pandas as pd
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 from sklearn.model_selection import train_test_split
 
-# Define your custom dataset class
+# Define a custom dataset class for PyTorch
 class ToxicDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
         self.encodings = encodings
@@ -18,20 +25,23 @@ class ToxicDataset(torch.utils.data.Dataset):
         item['labels'] = torch.tensor(self.labels[i])
         return item
 
-# Load the data
+# Load the data and split into training and validation sets
 data = pd.read_csv('train.csv')
 train_data, validation_data = train_test_split(data, test_size=0.2, random_state=42)
 
 # Initialize the tokenizer
 tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
 
+# Function to encode the input text
 def encode_inputs(batch):
     comments = batch['comment_text'].tolist()
     return tokenizer(comments, padding=True, truncation=True, max_length=512)
 
+# Tokenize and encode the training and validation data
 train_encoded_data = encode_inputs(train_data)
 validation_encoded_data = encode_inputs(validation_data)
 
+# Create custom PyTorch datasets for training and validation
 train_dataset = ToxicDataset(train_encoded_data, train_data.iloc[:, 2:].values)
 validate_dataset = ToxicDataset(validation_encoded_data, validation_data.iloc[:, 2:].values)
 
@@ -40,7 +50,7 @@ model_name = 'distilbert-base-uncased'
 config = DistilBertConfig.from_pretrained(model_name, num_labels=6)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, config=config)
 
-# Training arguments
+# Define training arguments
 train_arguments = TrainingArguments(
     evaluation_strategy="steps",
     output_dir="output",
@@ -51,7 +61,7 @@ train_arguments = TrainingArguments(
     logging_steps=100
 )
 
-# Trainer
+# Initialize the Hugging Face Trainer with the custom dataset, model, and training arguments
 trainer = Trainer(
     model=model,
     train_dataset=train_dataset,
@@ -59,13 +69,18 @@ trainer = Trainer(
     args=train_arguments
 )
 
+# Fine-tune the model
 trainer.train()
 
+# Save the tokenizer and model
 save_directory = 'saved'
 tokenizer.save_pretrained(save_directory)
 model.save_pretrained(save_directory)
+
+# Load the saved tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained(save_directory)
 model = AutoModelForSequenceClassification.from_pretrained(save_directory)
 
+# Evaluate the model and print the results
 results = trainer.evaluate()
 print(results)
